084a3897c2b10ad785a4e5d794f859981ad30cec
Rahul Kumar <rahul@thoughtnirvana.com>
2018-08-06 21:45:37 +0530

    Merged dev and prod config

--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,14 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 var ManifestPlugin = require('webpack-manifest-plugin')
 const webpack = require('webpack')
+const merge = require('webpack-merge')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const constants = require('./constants')
+const env = process.env.NODE_ENV
 
 
-module.exports = {
+let config = {
     entry: {
         bundle: ['./src/scripts/app.js'],
     },
@@ -37,3 +40,41 @@ module.exports = {
         new ManifestPlugin(),
     ],
 }
+
+
+if (env == 'production') {
+    config = merge.smart(config, {
+        mode: 'production',
+        output: {
+            filename: '[name].[chunkhash].js',
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production')
+            }),
+        ],
+        optimization: {
+            runtimeChunk: 'single',
+            splitChunks: {
+                chunks: 'all',
+            },
+            minimizer: [
+                new UglifyJsPlugin({}),
+            ],
+        }
+    })
+
+} else {
+    config = merge.smart(config, {
+        mode: 'development',
+        devtool: 'inline-source-map',
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('development')
+            }),
+        ],
+    })
+
+}
+
+module.exports = config
