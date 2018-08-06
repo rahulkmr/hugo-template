af095403973b00545918af6f2c464c8802cf5c5d
Rahul Kumar <rahul@thoughtnirvana.com>
2018-08-06 23:40:31 +0530

    Added tasks for fonts and webpack bundle

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,20 @@
 import gulp from 'gulp'
+import flatten from 'gulp-flatten'
 import sourcemaps from 'gulp-sourcemaps'
 import postcss from 'gulp-postcss'
 import postcssPresetEnv from 'postcss-preset-env'
 import postcssImport from 'postcss-import'
-import constants from './constants'
 import BrowserSync from 'browser-sync'
+import webpack from 'webpack'
+import logger from 'fancy-log'
+
+import constants from './constants'
+import webpackConfig from './webpack.config.js'
+
 
 const browserSync = BrowserSync.create()
 const stylesGlob = './src/styles/**/*.css'
+const fontsGlob = './src/styles/**/*'
 
 
 gulp.task('develop', ['styles'], (done) => runServer(done))
@@ -22,6 +29,26 @@ gulp.task('styles', () =>
         .pipe(gulp.dest(`./${constants.buildPath}/styles`))
 )
 
+
+gulp.task('scripts', (done) => {
+  webpack(webpackConfig, (error, stats) => {
+      if (error)
+          throw new Error(error)
+    logger.info("[webpack]", stats.toString({
+        colors: true,
+        progress: true
+    }))
+    done()
+  })
+})
+
+
+gulp.task('fonts', () => (
+  gulp.src(fontsGlob)
+    .pipe(flatten())
+    .pipe(gulp.dest(`./${constants.buildPath}/fonts`))
+    .pipe(browserSync.stream())
+))
 
 const runServer = () => {
     browserSync.init({
