513917bb8983d35e178f9bac6844bce34875b6e3
Rahul Kumar <rahul@thoughtnirvana.com>
2018-08-06 21:30:16 +0530

    postcss import and env

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,17 @@
 import gulp from 'gulp'
+import sourcemaps from 'gulp-sourcemaps'
 import postcss from 'gulp-postcss'
-import constants from 'constants'
+import postcssPresetEnv from 'postcss-preset-env'
+import postcssImport from 'postcss-import'
+import constants from './constants'
 import BrowserSync from 'browser-sync'
 
 const browserSync = BrowserSync.create()
 
 gulp.task('styles', () =>
     gulp.src('./src/styles/*.css')
-        .pipe(postcss())
+        .pipe(sourcemaps.init())
+        .pipe(postcss([postcssImport(), postcssPresetEnv()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(`./${constants.buildPath}/styles`))
-        .pipe(browserSync.stream())
 )
