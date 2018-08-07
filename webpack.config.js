0888d6a2a7ddcde57cb439f3999b4c2b42de0716
Rahul Kumar <rahul@thoughtnirvana.com>
2018-08-07 14:40:01 +0530

    Output assets to hugo static directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 var ManifestPlugin = require('webpack-manifest-plugin')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const constants = require('./constants')
 const env = process.env.NODE_ENV
 
 
 let config = {
     entry: {
-        bundle: ['./src/scripts/app.js'],
+        bundle: ['./assets/scripts/app.js'],
     },
     output: {
-        filename: '[name].[hash].js',
-        path: path.resolve(__dirname, constants.buildPath),
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'site/static/assets'),
     },
     module: {
         rules: [
@@ -35,38 +33,34 @@ let config = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/webpack_bundle.html',
-            filename: constants.webpackScriptsBundle,
-        }),
         new webpack.HashedModuleIdsPlugin(),
-        new ManifestPlugin(),
+        new ManifestPlugin({
+            fileName: '../../data/assets.json',
+        }),
     ],
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
 }
 
 
 if (env === 'production') {
     config = merge.smart(config, {
         mode: 'production',
-        output: {
-            filename: '[name].[chunkhash].js',
-        },
         plugins: [
             new webpack.DefinePlugin({
                 'process.env.NODE_ENV': JSON.stringify('production')
             }),
         ],
         optimization: {
-            runtimeChunk: 'single',
-            splitChunks: {
-                chunks: 'all',
-            },
             minimizer: [
                 new UglifyJsPlugin({}),
             ],
-        }
+        },
     })
-
 } else {
     config = merge.smart(config, {
         mode: 'development',
