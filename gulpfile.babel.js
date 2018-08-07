db08c42f8c6d20b6cbd5982d536cb8a6c604847f
Rahul Kumar <rahul@thoughtnirvana.com>
2018-08-07 19:55:05 +0530

    Fix unnecessary build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -19,8 +19,7 @@ const scriptsGlob = './assets/scripts/**/*.js'
 const fontsGlob = './assets/fonts/**/*'
 const imagesGlob = './assets/images/**/*'
 const hugoRoot = './site'
-const hugoGlob = `./${hugoRoot}/**/*`
-const assetsManifest = `./${hugoRoot}/data/assets.json`
+const hugoGlob = [`./${hugoRoot}/**/*`, `!./${hugoRoot}/resources/_gen/**/*`]
 const hugoDefaultArgs = ['--destination', `../${buildDir}`, '--source', 'site', '-v']
 const hugoPreviewArgs = hugoDefaultArgs.concat(['--buildDrafts', '--buildFuture'])
 const assetsTasks = ['clean', 'scripts', 'styles', 'fonts', 'images']
@@ -28,26 +27,26 @@ const assetsTasks = ['clean', 'scripts', 'styles', 'fonts', 'images']
 
 gulp.task('clean', () => del('dist'))
 
-gulp.task('build', assetsTasks, (done) => runHugo(done, hugoDefaultArgs))
-gulp.task('build-preview', assetsTasks, (done) => runHugo(done, hugoPreviewArgs))
-gulp.task('develop', assetsTasks.concat['build'], () => runServer())
-gulp.task('develop-preview', assetsTasks.concat['build-preview'], () => runServer())
+gulp.task('build', assetsTasks, (done) => runHugo(hugoDefaultArgs, done))
+gulp.task('build-preview', assetsTasks, (done) => runHugo(hugoPreviewArgs, done))
+gulp.task('develop', ['build'], () => runServer())
+gulp.task('develop-preview', ['build-preview'], () => runServer())
 gulp.task('default', ['develop'])
 
-gulp.task('hugo', (done) => runHugo(done, hugoDefaultArgs))
+gulp.task('hugo', (done) => runHugo(hugoDefaultArgs, done))
 
 gulp.task('styles', ['clean'], () =>
     gulp.src(stylesGlob)
         .pipe(sourcemaps.init())
         .pipe(postcss())
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./site/static/styles'))
+        .pipe(gulp.dest(`./${buildDir}/styles`))
         .pipe(browserSync.stream())
 )
 
 
 gulp.task('scripts', ['clean'], (done) => {
-    del('./site/static/assets')
+    del(`./${buildDir}/assets`)
     webpack(webpackConfig, (error, stats) => {
         if (error) {
             throw new Error(error)
@@ -62,19 +61,20 @@ gulp.task('scripts', ['clean'], (done) => {
 })
 
 
-gulp.task('fonts', ['clean'], () => (
+gulp.task('fonts', ['clean'], () =>
     gulp.src(fontsGlob)
         .pipe(flatten())
-        .pipe(gulp.dest('./site/static/fonts'))
+        .pipe(gulp.dest(`./${buildDir}/fonts`))
         .pipe(browserSync.stream())
-))
+)
 
 
 gulp.task('images', ['clean'], () =>
     gulp.src(imagesGlob)
         .pipe(imagemin())
-        .pipe(gulp.dest('./site/static/images'))
-        .pipe(browserSync.stream()))
+        .pipe(gulp.dest(`./${buildDir}/images`))
+        .pipe(browserSync.stream())
+)
 
 
 const runServer = () => {
@@ -88,18 +88,17 @@ const runServer = () => {
     gulp.watch(fontsGlob, ['fonts'])
     gulp.watch(imagesGlob, ['images'])
     gulp.watch(hugoGlob, ['hugo'])
-    gulp.watch(assetsManifest, ['hugo'])
 }
 
 
-const runHugo = (done, args) => {
+const runHugo = (args, done) => {
   return spawn('hugo', args, {stdio: "inherit"}).on("close", (returnCode) => {
     if (returnCode === 0) {
       browserSync.reload()
-      done()
+      done && done()
     } else {
       browserSync.notify("Hugo build failed.")
-      done("Hugo build failed.")
+      done && done("Hugo build failed.")
     }
   })
 }
